4d65470b497badf78632d00cd8977420aa45987f
sushma <sushma@kitintellect.com>
2025-09-19 15:14:57 +0530

    project delete, update, showbyid, listall

---
+++ src/api/projects.ts
@@ -0,0 +1,26 @@
+import api from "./axios";
+
+export const getProjects = async () => {
+  const res = await api.get("/api/projects");
+  return res.data;
+};
+
+export const getProjectById = async (id: number) => {
+  const res = await api.get(`/api/projects/${id}`);
+  return res.data;
+};
+
+export const createProject = async (project: unknown) => {
+  const res = await api.post("/api/projects", project);
+  return res.data;
+};
+
+export const updateProject = async (id: number, project: unknown) => {
+  const res = await api.put(`/api/projects/${id}`, project);
+  return res.data;
+};
+
+export const deleteProject = async (id: number) => {
+  const res = await api.delete(`/api/projects/${id}`);
+  return res.data;
+};
